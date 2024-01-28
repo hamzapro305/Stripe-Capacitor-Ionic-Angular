4d44394e1cb290a90d68837811958753b8583710
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2024-01-28 23:09:12 +0500

    fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  stripe: {
+    publishableKey:
+      'pk_test_51M1ru6ALO2GIO6ULoBrPguH6jabDb9go8nesd5AXIXGuHGiE1jQuVHhojmO98CesiS85Wd5v7K7XAbpwXgV6fmlT00L8JJC6N6',
+    secretKey:
+      'sk_test_51M1ru6ALO2GIO6ULds10yrj31OB4aCXmSowmWDohlHuuThvIyFTd38xI9A74FgSapTBWhgSRGUKx6cm6nuOSyzZy00RiDD56qp',
+  },
+  api: 'http://localhost:3000/',
 };
